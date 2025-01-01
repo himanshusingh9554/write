ce211350ac2e5243aba807f691939afab37ea066
himanshusingh9554 <himanshusingh9554@gmail.com>
2025-01-01 14:10:35 +0530

    add clean code

--- app.js
+++ app.js
@@ -1,50 +1,7 @@
 const http=require('http')
-const fs=require("fs")
+const routes=require("./routes")
 const server = http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-
-    if(req.url==='/'){
-        res.setHeader("Content-type","text/html");
-
-        res.end(
-            `
-            <form action="/message" method="POST">
-                <label>Name:</label>
-                <input type="text" name = "username"></input>
-                <button type="submit">Add</button>
-            </form>
-            `
-        );
-    }else{
-        if(req.url==="/message"){
-            let body=[];
-            req.on("data",(chunks)=>{
-                body.push(chunks);
-
-            });
-            req.on("end",()=>{
-                let buffer=Buffer.concat(body);
-                console.log(buffer);
-                let formData=buffer.toString();
-                console.log(formData);
-                const formValues= formData.split('=')[1];
-                fs.writeFile('formValues.txt',formValues, (err)=>{
-                    res.statusCode=302;
-                    res.setHeader('Location','/');
-                    res.end();
-                })
-            })
-        }else{
-            if(req.url=='/read'){
-                fs.readFile('formValues.txt',(err,data)=>{
-                    console.log(data.toString());
-                    res.end(`
-                        <h1>${data.toString()}</h1>`);
-                })
-            }
-        }
-    }
+   
 });
 server.listen(3000,()=>{
     console.log("server is running");
